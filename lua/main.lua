a98c5b744a4daaabb2e67ba0b19797256e4201ec
drumfeet <drumfeet@gmail.com>
2024-11-07 18:55:14 +0100

    wip

--- lua/main.lua
+++ lua/main.lua
@@ -31,108 +31,100 @@ end
 Records = Records or {}
 WaitFor = WaitFor or {}
 
+local function isSenderWaiting(sender)
+    return WaitFor[sender] ~= nil
+end
+
 Handlers.add("Create", Handlers.utils.hasMatchingTag("Action", "Create"), function(msg)
     local success, err = pcall(function()
-        -- Check if the sender is already waiting for a response
-        for _, v in ipairs(WaitFor) do
-            if v == msg.From then
-                sendErrorMessage(msg, 'You already have a market in progress!')
-                return
-            end
+        if isSenderWaiting(msg.From) then
+            sendErrorMessage(msg, 'You already have a market in progress!')
+            return
         end
 
-        -- prevent multiple creations while waiting for response
-        table.insert(WaitFor, msg.From)
+        -- Prevent multiple creations while waiting for a response
+        WaitFor[msg.From] = true
 
         ao.spawn(ao.env.Module.Id, { Tags = { Authority = ao.authorities[1] } })
         local childProcessId = Receive({ Action = "Spawned" }).Process
         print("childProcessId: " .. childProcessId)
 
-        -- Apply Template to childProcessId
-        -- Set Owner to empty string
         ao.send({
             Target = childProcessId,
             Action = "Eval",
-            Data = [[
-            Creator = Creator or ""
+            Data = string.format([[
+                Creator = Creator or ""
 
-            Handlers.add("GetProcessOwner", Handlers.utils.hasMatchingTag("Action", "GetProcessOwner"), function(msg)
-                print("GetProcessOwner")
-                print("ao.env.Process.Owner: " .. ao.env.Process.Owner)
-                ao.send({
-                    Target = msg.From,
-                    Data = ao.env.Process.Owner
-                })
-            end)
+                Handlers.add("GetProcessOwner", Handlers.utils.hasMatchingTag("Action", "GetProcessOwner"), function(msg)
+                    print("GetProcessOwner")
+                    print("ao.env.Process.Owner: " .. ao.env.Process.Owner)
+                    ao.send({
+                        Target = msg.From,
+                        Data = ao.env.Process.Owner
+                    })
+                end)
 
-            Handlers.add("GetOwner", Handlers.utils.hasMatchingTag("Action", "GetOwner"), function(msg)
-                print("GetOwner")
-                print("Owner: " .. Owner)
-                ao.send({
-                    Target = msg.From,
-                    Data = Owner
-                })
-            end)
+                Handlers.add("GetOwner", Handlers.utils.hasMatchingTag("Action", "GetOwner"), function(msg)
+                    print("GetOwner")
+                    print("Owner: " .. Owner)
+                    ao.send({
+                        Target = msg.From,
+                        Data = Owner
+                    })
+                end)
 
-            Handlers.add("GetCreator", Handlers.utils.hasMatchingTag("Action", "GetCreator"), function(msg)
-                print("GetCreator")
-                print("Creator: " .. Creator)
-                ao.send({
-                    Target = msg.From,
-                    Data = Creator
-                })
-            end)
+                Handlers.add("GetCreator", Handlers.utils.hasMatchingTag("Action", "GetCreator"), function(msg)
+                    print("GetCreator")
+                    print("Creator: " .. Creator)
+                    ao.send({
+                        Target = msg.From,
+                        Data = Creator
+                    })
+                end)
 
-            Handlers.add("Deposit", Handlers.utils.hasMatchingTag("Action", "Deposit"), function(msg)
-                print("Deposit")
-                ao.send({
-                    Target = msg.From,
-                    Data = "Deposit"
-                })
-            end)
+                Handlers.add("Deposit", Handlers.utils.hasMatchingTag("Action", "Deposit"), function(msg)
+                    print("Deposit")
+                    ao.send({
+                        Target = msg.From,
+                        Data = "Deposit"
+                    })
+                end)
 
-            Handlers.add("Withdraw", Handlers.utils.hasMatchingTag("Action", "Withdraw"), function(msg)
-                print("Withdraw")
-                ao.send({
-                    Target = msg.From,
-                    Data = "Withdraw"
-                })
-            end)
+                Handlers.add("Withdraw", Handlers.utils.hasMatchingTag("Action", "Withdraw"), function(msg)
+                    print("Withdraw")
+                    ao.send({
+                        Target = msg.From,
+                        Data = "Withdraw"
+                    })
+                end)
 
-            Handlers.add("Conclude", Handlers.utils.hasMatchingTag("Action", "Conclude"), function(msg)
-                print("Conclude")
-                ao.send({
-                    Target = msg.From,
-                    Data = "Conclude"
-                })
-            end)
-            Creator = "]] .. msg.From .. [["
-            Owner = ""
-        ]]
+                Handlers.add("Conclude", Handlers.utils.hasMatchingTag("Action", "Conclude"), function(msg)
+                    print("Conclude")
+                    ao.send({
+                        Target = msg.From,
+                        Data = "Conclude"
+                    })
+                end)
+
+                Creator = "]] .. msg.From .. [["
+                Owner = ""
+            ]]
+            )
         })
 
-        -- Store childProcessId in Records
-        Records[msg.From] = Records[msg.From] or {} -- Initialize if not exists
-        table.insert(Records[msg.From], childProcessId)
+        Records[msg.From] = Records[msg.From] or {}
+        Records[msg.From][#Records[msg.From] + 1] = childProcessId
 
-        -- Send response to the sender
-        ao.send({
-            Target = msg.From,
-            Data = "Market Created!"
-        })
+        ao.send({ Target = msg.From, Data = "Market Created!" })
 
-        -- Remove sender from WaitFor
-        for i, v in ipairs(WaitFor) do
-            if v == msg.From then
-                table.remove(WaitFor, i)
-                break
-            end
-        end
+        WaitFor[msg.From] = nil -- Remove sender from WaitFor set
     end)
+
     if not success then
         sendErrorMessage(msg, 'An unexpected error occurred: ' .. tostring(err))
     end
 end)
+
 
 Handlers.add("List", Handlers.utils.hasMatchingTag("Action", "List"), function(msg)
     print("List")
