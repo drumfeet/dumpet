5a28467d90cd7f355e6e9114f648fe4035eeb7ef
drumfeet <drumfeet@gmail.com>
2024-11-21 18:25:31 +0100

    w

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -26,6 +26,7 @@ import {
 import AppHeader from "@/components/AppHeader"
 import { useAppContext } from "@/context/AppContext"
 import ShareIcon from "@/components/icons/ShareIcon"
+import Head from "next/head"
 
 export async function getStaticPaths() {
   return { paths: [], fallback: "blocking" }
@@ -175,8 +176,30 @@ export default function Home({ _id = null }) {
     }
   }
 
+  const meta = {
+    title: "dumpet.fun",
+    description: "Populartiy contest",
+    image: "T2q7IO67TYEhuk1CIPVxHX9MdEmzTUocZjScmdWTHK0",
+  }
+
   return (
     <ChakraProvider>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={`${meta.title}`} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta
+          name="twitter:image"
+          content={`https://arweave.net/${meta.image}`}
+        />
+        <meta property="og:title" content={`${meta.title}`} />
+        <meta name="og:description" content={meta.description} />
+        <meta name="og:image" content={`https://arweave.net/${meta.image}`} />
+        <link rel="icon" href=".././favicon.svg" />
+      </Head>
       <Flex
         direction="column"
         align="center"
