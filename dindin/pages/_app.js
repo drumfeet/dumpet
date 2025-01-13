d9bd300c182a898d2bd8e137c3cd902f47acd9a4
drumfeet <drumfeet@gmail.com>
2025-01-13 23:36:21 +0100

    code refactor

--- dindin/pages/_app.js
+++ dindin/pages/_app.js
@@ -1,7 +1,7 @@
 import { ArNext } from "arnext"
 import "@/styles/globals.css"
 import "@/styles/animations.css"
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App(props) {
   return (
