f6095d1ff3db5efd3a02e2d989ea116227259175
drumfeet <drumfeet@gmail.com>
2025-01-14 17:46:32 +0100

    re-arrange token list

--- next-app/components/TokenList.js
+++ next-app/components/TokenList.js
@@ -1,9 +1,9 @@
 const tokenList = [
-  { value: "QD3R6Qes15eQqIN_TK5s7ttawzAiX8ucYI2AUXnuS18", label: "DUMPET" },
   { value: "NG-0lVX882MG5nhARrSzyprEK6ejonHpdUmaaMPsHE8", label: "qAR" },
   { value: "xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10", label: "wAR" },
   { value: "7zH9dlMNoxprab9loshv3Y7WG45DOny_Vrq9KrXObdQ", label: "wUSDC" },
   { value: "wOrb8b_V8QixWyXZub48Ki5B6OIDyf_p1ngoonsaRpQ", label: "TRUNK" },
+  { value: "QD3R6Qes15eQqIN_TK5s7ttawzAiX8ucYI2AUXnuS18", label: "DUMPET" },
   { value: "custom", label: "Input custom value" },
 ]
 
