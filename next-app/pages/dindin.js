1732594453e83f38a68bbdff4812ef44fa0c5334
drumfeet <drumfeet@gmail.com>
2025-01-20 15:19:46 +0100

    import next/navigation instead

--- next-app/pages/dindin.js
+++ next-app/pages/dindin.js
@@ -1,11 +1,11 @@
 import { useEffect } from "react"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 
 export default function RedirectPage() {
   const router = useRouter()
 
   useEffect(() => {
-    router.replace("https://dindin.dumpet.fun/")
+    router.replace("https://dindin.dumpet.fun")
   }, [router])
 
   return null // No content is displayed on this page
